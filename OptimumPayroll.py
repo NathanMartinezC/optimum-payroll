e138029b0a57281275c7bf6864748c1fada31a5d
Nathán <n.mtz.cas@gmail.com>
2021-05-21 17:53:18 -0500

    complete version

--- OptimumPayroll.py
+++ OptimumPayroll.py
@@ -7,7 +7,6 @@ class PayrollItem:
     additionalFactor = 0
     isr = 0
     J = 0
-    phi = 0
     sbc = 0
     christmasBonus = 15
     vacations = 6
@@ -16,17 +15,34 @@ class PayrollItem:
     integrationFactor = 0
     dailySalary = 0
     subsidy = 0
+    upperSubsidy = 7382.34
     uma = 86.88
+    minimumSalary = 123.22*30
+    companyFactor = 0
+    salaryCompany = 0
+    phi = 0
 
-    def __init__(self, salary, payrollDaysFactor):
+    imss = 0
+    totalTaxes = 0
+    payrollPay = 0
+
+    def __init__(self, salary, companyFactor, payrollDaysFactor):
         self.salary = salary
         self.payrollDaysFactor = payrollDaysFactor
+        self.companyFactor = companyFactor
+        self.salaryCompany = salary*companyFactor
         
         #Initialization
         self.getIsrFactors()
         self.getIsr()
         self.getSbc()
         self.getSubsidy()
+        self.imss = ImssTax(self.sbc,self.uma,self.payrollDaysFactor,self.salaryCompany)
+        self.totalTaxes = self.imss.totalTax + self.isr - self.subsidy
+        self.payrollPay = self.salary + self.imss.totalEmployerTax + self.isr - self.subsidy
+        self.CHM()
+        self.OF()
+
 
     
     def getIsrFactors(self):
@@ -37,7 +53,7 @@ class PayrollItem:
 
         for i, lower in enumerate(lowerLimitsIsr):
             upper = upperLimitsIsr[i]
-            if self.salary >= lower and self.salary <= upper:
+            if self.salaryCompany >= lower and self.salaryCompany <= upper:
                 self.lowerIsr = lowerLimitsIsr[i]
                 self.upperIsr = upperLimitsIsr[i]
                 self.fixedTax = fixedTaxesIsr[i]
@@ -48,11 +64,11 @@ class PayrollItem:
         
 
     def getIsr(self):
-        self.isr = (self.salary - self.lowerIsr) * self.additionalFactor + self.fixedTax
+        self.isr = (self.salaryCompany - self.lowerIsr) * self.additionalFactor + self.fixedTax
 
     def getSbc(self):
         self.integrationFactor = (1/self.quotedDays) * (self.vacations * self.vacationFactor + self.quotedDays + self.christmasBonus)
-        self.dailySalary = self.salary/self.payrollDaysFactor
+        self.dailySalary = self.salaryCompany/self.payrollDaysFactor
         self.sbc = self.dailySalary * self.integrationFactor
 
     def getSubsidy(self):
@@ -62,7 +78,7 @@ class PayrollItem:
 
         for i, lower in enumerate(lowerLimitSubsidy):
             upper = upperLimitSubsidy[i]
-            if self.salary >= lower and self.salary <= upper:
+            if self.salaryCompany >= lower and self.salaryCompany <= upper:
                 self.lowerSubsidy = lowerLimitSubsidy[i]
                 self.upperSubsidy = upperLimitSubsidy[i]
                 self.subsidy = subsidyValue[i]
@@ -70,38 +86,250 @@ class PayrollItem:
             else:
                 pass
 
-    def OF(self):
-        self.J = abs(self.isr - self.subsidy) + (self.sbc - 3*self.uma) * self.additionalFactor
+    def CHM(self):
+        g1 = self.salaryCompany - self.upperSubsidy
+        g2 = self.minimumSalary - self.salaryCompany
 
-    # def CMH(self):
-    #     g1 = 0
-    #     g2 = 0
+        if(g1 <= 0):
+            pass
+        else:
+            self.phi += 1
+        if(g2 <= 0):
+            pass
+        else:
+            self.phi += 1
+
+
+    def OF(self):
+        self.J = abs(self.isr - self.subsidy) #self.payrollPay  #abs(self.isr - self.subsidy) + self.imss.totalEmployerTax + self.imss.totalEmployeeTax + self.salary
+
 
     def printValues(self):
+        print(" ")
         print("Salary: ", self.salary)
+        print("Salary company: ", self.salaryCompany)
         print("ISR: ", self.isr)
-        print("ISR - Lower Limit: ", self.lowerIsr)
-        print("ISR - Upper Limit: ", self.upperIsr)
-        print("")
         print("Subsidy: ", self.subsidy)
-        print("Subsidy - Lower Limit: ", self.lowerSubsidy)
-        print("Subsidy - Upper Limit: ", self.upperSubsidy)
-        print("")
         print("SBC: ", self.sbc)
+        print("IMSS - Employee: ", self.imss.totalEmployeeTax)
+        print("IMSS - Employer: ", self.imss.totalEmployerTax)
+        print("Total IMSS Tax: ", self.imss.totalTax)
+        print('Total Payroll: ', self.payrollPay)
         print("J: ", self.J)
-        print("Constraints: ", self.phi)
+        print('phi: ', self.phi)
+        print("Company Factor: ", self.companyFactor)
+        print(" ")
 
-import numpy as np
+class ImssTax:
+    sbc = 0
+    uma = 0
+    payrollDays = 0
+    salary = 0
+    totalEmployeeTax = 0
+    totalEmployerTax = 0
+    totalTax = 0
 
-a = np.arange(15)
+    #Employee Tax
+    sicknessEmployeeTax = 0
+    fixedEmployeeTax = 0
+    medicalEmployeeTax = 0
+    moneyEmployeeTax = 0
+    additionalEmployeeTax = 0
+    disabilityEmployeeTax = 0
+    retirementEmployeeTax = 0
+    ceavEmployeeTax = 0
 
+    #Employee Tax Factors
+    #sicknessEmployeeFactor = 0
+    fixedEmployeeFactor = 0
+    medicalEmployeeFactor = 0.00375
+    moneyEmployeeFactor = 0.0025
+    additionalEmployeeFactor = 0.004
+    disabilityEmployeeFactor = 0.00625
+    retirementEmployeeFactor = 0
+    ceavEmployeeFactor = 0.01125
+
+    #Employer Tax
+    sicknessEmployerTax = 0
+    fixedEmployerTax = 0
+    medicalEmployerTax = 0
+    moneyEmployerTax = 0
+    additionalEmployerTax = 0
+    disabilityEmployerTax = 0
+    retirementEmployerTax = 0
+    ceavEmployerTax = 0
+    riskEmployerTax = 0
+    infonavitTax = 0
+    kinderEmployerTax = 0
+
+    #Employer Tax Factors
+    #sicknessEmployerFactor = 0
+    fixedEmployerFactor = 0.2040
+    medicalEmployerFactor = 0.0105
+    moneyEmployerFactor = 0.007
+    additionalEmployerFactor = 0.0110
+    disabilityEmployerFactor = 0.0175
+    retirementEmployerFactor = 0.02
+    ceavEmployerFactor = 0.03150
+    riskEmployerFactor = 0.005
+    infonavitFactor = 0.05
+    kinderEmployerFactor = 0.01
+
+    def __init__(self,sbc,uma,payrollDays,salary):
+        self.sbc = sbc
+        self.uma = uma
+        self.payrollDays = payrollDays
+        self.salary = salary
+
+        self.totalEmployee()
+        self.totalEmployer()
+        self.totalTax = self.totalEmployeeTax + self.totalEmployerTax
+
+    def totalEmployee(self):
+        self.sicknessEmployee()
+        self.disabilityEmployee()
+        self.retirementEmployee()
+        self.ceavEmployee()
+
+        self.totalEmployeeTax = self.sicknessEmployeeTax + self.disabilityEmployeeTax + self.retirementEmployeeTax + self.ceavEmployeeTax
+
+    def totalEmployer(self):
+        self.sicknessEmployer()
+        self.riskEmployer()
+        self.disabilityEmployer()
+        self.retirementEmployer()
+        self.ceavEmployer()
+        self.kinderEmployer()
+        self.infonavitEmployer()
+
+        self.totalEmployerTax = self.sicknessEmployerTax + self.riskEmployerTax + self.disabilityEmployerTax + self.retirementEmployerTax + self.ceavEmployerTax + self.kinderEmployerTax + self.infonavitTax
+
+    ###### Methods of Employee Taxes  
+    def sicknessEmployee(self):
+        self.fixedEmployeeTax = self.uma * self.payrollDays * self.fixedEmployeeFactor
+        self.medicalEmployeeTax = self.uma * self.payrollDays * self.medicalEmployeeFactor
+        self.moneyEmployeeTax = self.uma * self.payrollDays * self.moneyEmployeeFactor
+
+        if (self.salary >= 3*self.uma*self.payrollDays):
+            self.additionalEmployeeTax = ((self.sbc - 3*self.uma)*self.additionalEmployeeFactor) * self.payrollDays
+        else:
+            self.additionalEmployeeTax = 0
+
+        self.sicknessEmployeeTax = self.fixedEmployeeTax + self.additionalEmployeeTax + self.medicalEmployeeTax + self.moneyEmployeeTax
+
+    def disabilityEmployee(self):
+        self.disabilityEmployeeTax = self.sbc * self.payrollDays * self.disabilityEmployeeFactor
+
+    def retirementEmployee(self):
+        self.retirementEmployeeTax = self.sbc * self.payrollDays * self.retirementEmployeeFactor
+    
+    def ceavEmployee(self):
+        self.ceavEmployeeTax = self.sbc * self.payrollDays * self.ceavEmployeeFactor
+    
+    ###### Methods of Employer Taxes
+    def sicknessEmployer(self):
+        self.fixedEmployerTax = self.uma * self.payrollDays * self.fixedEmployerFactor
+        self.medicalEmployerTax = self.uma * self.payrollDays * self.medicalEmployerFactor
+        self.moneyEmployerTax = self.uma * self.payrollDays * self.moneyEmployerFactor
+
+        if (self.salary >= 3*self.uma*self.payrollDays):
+            self.additionalEmployerTax = ((self.sbc - 3*self.uma)*self.additionalEmployerFactor) * self.payrollDays
+        else:
+            self.additionalEmployerTax = 0
+
+        self.sicknessEmployerTax = self.fixedEmployerTax + self.additionalEmployerTax + self.medicalEmployerTax + self.moneyEmployerTax
+
+    def riskEmployer(self):
+        self.riskEmployerTax = self.sbc * self.payrollDays * self.riskEmployerFactor
+
+    def disabilityEmployer(self):
+        self.disabilityEmployerTax = self.sbc * self.payrollDays * self.disabilityEmployerFactor
+
+    def retirementEmployer(self):
+        self.retirementEmployerTax = self.sbc * self.payrollDays * self.retirementEmployerFactor
+    
+    def ceavEmployer(self):
+        self.ceavEmployerTax = self.sbc * self.payrollDays * self.ceavEmployerFactor
+
+    def kinderEmployer(self):
+        self.kinderEmployerTax = self.sbc * self.payrollDays * self.kinderEmployerFactor
+
+    def infonavitEmployer(self):
+        self.infonavitTax = self.sbc * self.payrollDays * self.infonavitFactor
 
     
+import numpy as np
+
+NP = 10
+GMAX = 60
+p_min = 0.6
+p_max = 1.0
+salary = 8000
+payrollDaysFactor = 30.42
 
 
+P = []
+V = []
+p = []
+v = []
 
+#Inicializacion de la poblacion
+for i in range(NP):
+    companyFactor = p_min + np.random.rand() * (p_max - p_min)
+    p.append(PayrollItem(salary, companyFactor, payrollDaysFactor))
+    v.append(PayrollItem(0, 0, payrollDaysFactor))
 
+for i in range(GMAX):
+    F = np.random.rand()
+    CR = np.random.rand()
+    
+    while (((F < 0.3) or (F > 0.9)) and ((CR < 0.8) or (CR >= 1))):
+        F = np.random.rand()
+        CR = np.random.rand()
 
-p = PayrollItem(5000, 30.42)
-p.printValues()
-# print(p.isr())
+    for k in range(NP):
+        flag = True
+
+        while flag == True:
+            r1 = np.random.randint(0,NP)
+            r2 = np.random.randint(0,NP)
+            r3 = np.random.randint(0,NP)
+            if (r1 != k and r2 != k and r3 != k) and ( r1 != r2 and r2 != r3 and r1 !=r3):
+                flag = False
+        
+        flag = True
+        
+        if np.random.rand() < CR:
+            u = PayrollItem(salary, p[r1].companyFactor + F*(p[r2].companyFactor - p[r3].companyFactor),payrollDaysFactor)
+            if u.companyFactor < p_min or u.companyFactor > p_max:
+                u.companyFactor = p_min + np.random.rand() * (p_max - p_min)
+            v[k] = PayrollItem(salary,u.companyFactor,payrollDaysFactor)
+        else:
+            v[k] = PayrollItem(salary,p[k].companyFactor,payrollDaysFactor)
+
+    for k in range(NP):
+
+        if (p[k].phi == 0) and (v[k].phi == 0):
+            if (p[k].J < v[k].J):
+                p[k] = PayrollItem(salary,p[k].companyFactor,payrollDaysFactor)
+            elif(p[k].J > v[k].J):
+                p[k] = PayrollItem(salary,v[k].companyFactor,payrollDaysFactor)
+        
+        elif(p[k].phi != 0) and (v[k].phi == 0):
+            p[k] = PayrollItem(salary,v[k].companyFactor,payrollDaysFactor)
+        elif(p[k].phi == 0) and (v[k].phi != 0):
+            p[k] = PayrollItem(salary,p[k].companyFactor,payrollDaysFactor)
+        else:
+            if(np.random.rand() < 0.5):
+                p[k] = PayrollItem(salary,v[k].companyFactor,payrollDaysFactor)
+            else:
+                p[k] = PayrollItem(salary,p[k].companyFactor,payrollDaysFactor)
+
+for i in p:
+    i.printValues()
+
+Z = PayrollItem(8000, 0.6, payrollDaysFactor)
+Z.printValues()
+
+#for p in population:
+#    p.printValues()
